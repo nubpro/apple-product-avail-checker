b5040f522a1c5362243c362e87d09d228a4e2a2a
Chai Woon Khai <chai1998@live.com>
2021-11-24 12:13:11 +0800

    fix: remove urls

--- src/functions/discord-publisher/app.js
+++ src/functions/discord-publisher/app.js
@@ -1,18 +1,7 @@
 import axios from "axios";
 
-const ENV_VARIABLES = {
-  test: {
-    discordWebhook:
-      "https://discord.com/api/webhooks/785812605917855744/hH7mcPbCpi7sTwBD-AHw-ZXTB03GtnarFWV-p9DIhSjrxdoN9mVA0_tLTSgNwpZK-_Q4",
-  },
-  prod: {
-    discordWebhook:
-      "https://discord.com/api/webhooks/783771622347374642/IuHBtsOSXxvCcJxgagi970-SpJoI9Yz12GUHXOAej21NTheAHAJy_m4C937JWznVmjS6",
-  },
-};
-
 export async function sendMessage(content) {
-  const discordWebhook = ENV_VARIABLES[process.env.ENV].discordWebhook;
+  const discordWebhook = process.env.discordWebhook;
 
   const body = {
     content,
