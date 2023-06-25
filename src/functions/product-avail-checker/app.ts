d9cb95785c9e3a1775e706802a2c5399789447c1
Chai Woon Khai <chai1998@live.com>
2023-06-26 02:13:28 +0800

    fix: improve text detection and query without using browser cache

--- src/functions/product-avail-checker/app.ts
+++ src/functions/product-avail-checker/app.ts
@@ -2,7 +2,14 @@ import cheerio from "cheerio";
 import axios, { AxiosResponse } from "axios";
 
 export const handler = async (event: { url: string }) => {
-  const { status, data }: AxiosResponse<string> = await axios.get(event.url);
+  const { status, data }: AxiosResponse<string> = await axios.get(event.url, {
+    // query URL without using browser cache
+    headers: {
+      "Cache-Control": "no-cache",
+      Pragma: "no-cache",
+      Expires: "0",
+    },
+  });
 
   if (status !== 200) {
     throw new Error(`Page is down. Status code ${status}`);
@@ -10,16 +17,16 @@ export const handler = async (event: { url: string }) => {
 
   const $ = cheerio.load(data);
   const button = $(".ac-ln-button").first();
-  const actionText = button.text().trim().split(" ")[0];
+  const actionText = button.text().toLowerCase();
   // TODO: don't compare with text, because different region uses different lang
   // note: Apple.com (US) will potentially use the word "order" instead of "buy",
   // this makes it difficult to differentiate using `data-analytics-title` attrib.
   // because it still uses 'shop' despite being able to purchase online
   // in other regions, 'shop' means the product is not yet available
 
-  const canBuy = actionText.toLowerCase() === "buy";
-  const canOrder = actionText.toLowerCase() === "order";
-  const canPreOrder = actionText.toLowerCase() === "pre-order";
+  const canBuy = actionText.includes("buy");
+  const canOrder = actionText.includes("order");
+  const canPreOrder = actionText.includes("pre-order");
 
   const isAvailable = canBuy || canOrder || canPreOrder;
 
